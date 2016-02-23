8a5d28e45f2c1a9d1f79fbe8be024f2851c12a92
avmSupport1 <support@avmap.gr>
2016-02-23 14:36:46 +0200

    Added files via upload

---
+++ src/search/encoding_fix.php
@@ -0,0 +1,20 @@
+
+<?php 
+function file_get_contents_utf8($fn)
+{
+    $content = file_get_contents($fn);
+    return mb_convert_encoding($content, 'UTF-8',mb_detect_encoding($content, 'UTF-8, ISO-8859-7', true));
+}
+/*
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('allow_url_fopen ','ON');
+$content = file_get_contents_utf8("http://maps.emeganisi.gr/mapserver/rest/styles/emeganisi_epixeiriseis.sld");
+
+$xml = simplexml_load_string($content);
+//print_r($xml);
+// for debug: 
+$dom = dom_import_simplexml($xml)->ownerDocument;
+$dom->formatOutput = true;
+echo $dom->saveXML();*/
+?>
